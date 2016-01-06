c02b882230684856ea705ff87a0454bacc252d81
Mika Lackman <mika.lackman@gmail.com>
2016-01-06 08:01:32 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name = "pymodel",
+  packages = ['model'],
+  version = "0.01",
+  description = "Model for web MVC",
+  author = "Mika Lackman",
+  author_email = "mika(at)gmail.com",
+  #url = "https://github.com/mlackman/pyavali/tree/master/pyavali",
+  keywords = ["web model MVC"],
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Alpha",
+    "Environment :: Other Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GPL",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
